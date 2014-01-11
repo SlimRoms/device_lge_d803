bf7e5f4397cfdd73d0f83dbda722cc5473d353e1
arcardinal <andrewrcardinal@gmail.com>
2014-01-11 13:23:13 -0600

    Update slim.mk for d803

--- slim.mk
+++ slim.mk
@@ -1,5 +1,5 @@
 #Release Name
-PRODUCT_RELEASE_NAME := d800
+PRODUCT_RELEASE_NAME := d803
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
@@ -12,13 +12,13 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/lge/d800/full_d800.mk)
+$(call inherit-product, device/lge/d803/full_d803.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := d800
-PRODUCT_NAME := slim_d800
+PRODUCT_DEVICE := d803
+PRODUCT_NAME := slim_d803
 PRODUCT_BRAND := LGE
-PRODUCT_MODEL := LG-D800
+PRODUCT_MODEL := LG-D803
 PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
