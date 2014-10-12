f08fb83448e50cd981e5fb90ce0cf8c643b24853
arcardinal <andrewrcardinal@gmail.com>
2014-10-12 12:58:23 -0500

    Cleanup

--- extract-files.sh
+++ extract-files.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
-# This script is maintained in the common tree
-../g2-common/extract-files.sh $*
+VENDOR=lge
+DEVICE=d800
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ `; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
--- full_d803.mk
+++ full_d803.mk
@@ -28,7 +28,7 @@ PRODUCT_NAME := full_d803
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D803
 PRODUCT_MANUFACTURER := lge
-PRODUCT_RESTRICT_VENDOR_FILES := true
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/d803/device.mk)
